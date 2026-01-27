4988839155b2e56f67060d6b319263a4ea5fb73f
ItsFill <dmks10312@gmail.com>
2026-01-27 21:48:41 +0100

    Added task 16(Cmake-training and creating file with console)

---
+++ 16-CMake-training/build/CMakeFiles/Cmake-training.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Cmake-training.dir/link.d"
+  "CMakeFiles/Cmake-training.dir/main.cpp.o"
+  "CMakeFiles/Cmake-training.dir/main.cpp.o.d"
+  "Cmake-training"
+  "Cmake-training.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Cmake-training.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ 16-CMake-training/build/CMakeFiles/Cmake-training.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for Cmake-training.
+# This may be replaced when dependencies are built.
